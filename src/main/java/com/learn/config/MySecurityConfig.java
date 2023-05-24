5aec3b921710500adb587a49f2b329dafdd1a0eb
dhrumildrc <dhrumil.bhalala@drcsystems.com>
2023-05-24 14:30:53 +0530

    Second commit after lunch

--- src/main/java/com/learn/config/MySecurityConfig.java
+++ src/main/java/com/learn/config/MySecurityConfig.java
@@ -1,4 +1,42 @@
 package com.learn.config;
 
-public class MySecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("john").password(this.passwordEncoder().encode("dhrumil")).roles("NORMAL");
+        auth.inMemoryAuthentication().withUser("roshni").password(this.passwordEncoder().encode("roshni")).roles("NORMAL","ADMIN");
+
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/public/**").hasRole("NORMAL")
+                .antMatchers("/users/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
--- src/main/java/com/learn/controllers/UserController.java
+++ src/main/java/com/learn/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.learn.controllers;
 import com.learn.models.User;
 import com.learn.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +15,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
+
     @GetMapping("/")
     public List<User> getAllUsers(){
         return this.userService.getAllUsers();
     }
 
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{username}")
     public User getUser(@PathVariable("username") String username){
         return this.userService.getUser(username);
