7b30d9933efbfb3a1672b74444afff3f94be2fcf
dhrumildrc <dhrumil.bhalala@drcsystems.com>
2023-05-24 11:35:16 +0530

    Project added to git

---
+++ src/main/java/com/learn/SpringBootSecurityLearnApplication.java
@@ -0,0 +1,17 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityLearnApplication.class, args);
+		System.out.println("Application Started.........");
+		System.out.println("Jay Shree Krishna");
+
+
+	}
+
+}
